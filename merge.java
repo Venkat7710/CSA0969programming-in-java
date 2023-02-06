1d44658637d9af59e5cdf839216837352ac5d749
Venkat7710 <113335953+Venkat7710@users.noreply.github.com>
2023-02-06 19:16:39 +0530

    Add files via upload

---
+++ merge.java
@@ -0,0 +1,16 @@
+import java.util.Arraylist;
+class merge
+{
+public static void main(String[] args)
+{
+Arraylist<integer>arr1=new arraylist[]
+arr1.add(1,2,4,5);
+Arraylist<integer>arr2=new arraylist[]
+arr2  .add(2,4,6,8);
+Arraylist<integer>result=new arraylist[]
+result.addAll(arr1);
+result.addAll(arr2);
+collection.sort(result);
+System.out.println("merged sort:"+result);
+}
+}
