955393776782075a820fe427dc18ec81b0c28dd4
Venkat7710 <113335953+Venkat7710@users.noreply.github.com>
2023-02-06 17:20:17 +0530

    Add files via upload

---
+++ square.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class square
+{
+public static void main(String[] args)
+{
+Scanner sc=new Scanner(system.in);
+System.out.println("enter row and col");
+int row=sc.nextInt();
+int col=sc.nextInt();
+System.out.println("input a character");
+char a=sc.next().charAt(0);
+for(int i=1;i<=row;i++)
+{
+for(int j=1;j<=col;j++)
+{
+if((i==1||i==col)||(j==1||j==col));
+System.out.println(a);
+ifelse;
+System.out.println(" ");
+}
+}
+
+
